749d65f03b2379791e0072b80ed4ca0c6505a73b
shinnihon.toake <shinnihontoake@gmail.com>
2020-12-31 12:44:36 +0900

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Tasklist</h1>
-        </div>
-    </div>
+
+
+@if(count($tasks) > 0)
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>タスク</th>
+                <th>ステータス</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task -> id, ['task'=>$task -> id]) !!}</td>                 
+                    <td>{{ $task -> content }}</td>
+                    <td>{{ $task -> status }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endif
+
+{!! link_to_route('tasks.create', '新規タスク投稿', [],  ['class' => 'btn btn-primary']) !!}
+
+
 @endsection
